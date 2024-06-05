45d21aa49568d6794a6cd3352c31cb16752137e1
LuAsiNomas <163897528+LuAsiNomas@users.noreply.github.com>
2024-06-05 15:30:51 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function evento(e){
+    let hijo = e.target.parentElement.children;
+    let clase = e.target;
+    clase.className = "boxActivo";
+    
+    for (let i = 0; i < hijo.length; i++) {
+        if (hijo[i].id != e.target.id) {
+            hijo[i].className = "box";
+        }   
+    }
+}
